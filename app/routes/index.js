eb07282398a658ee6fe618aa306011b7e3fb03b0
Brad Pizzimenti <brad@gennakersystems.com>
2016-04-03 18:38:57 -0700

    install firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,7 @@
 import Ember from 'ember';
 
-var questions = [{
-  id: 1,
-  author: "brad",
-  question: "Will this message appear?",
-  answers: [{
-    id: 1,
-    qID: 1,
-    author: "bill",
-    body: "Yes it will!"
-  }]
-}];
-
-
 export default Ember.Route.extend({
   model() {
-    return questions;
+    return this.store.findAll('question');
   }
 });
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-msg-board',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://ember-msg-board.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
