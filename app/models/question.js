182c39a12da3b843dbf9e7bca222a5ccda37a70f
Brad Pizzimenti <brad@gennakersystems.com>
2016-04-09 09:48:12 -0700

    display questions and answers on the main page

--- app/models/question.js
+++ app/models/question.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     author: DS.attr(),
-    question: DS.attr()
+    question: DS.attr(),
+    answers: DS.hasMany('answer', {async: true})
 });
